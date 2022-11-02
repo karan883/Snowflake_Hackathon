53d0e211612a29546337f2d0970e5247fbc1bf03
Karan Chhabra <70219414+karan883@users.noreply.github.com>
2022-11-02 12:41:46 +0530

    Update SnowflakeApp.py

--- SnowflakeApp.py
+++ SnowflakeApp.py
@@ -16,23 +16,26 @@ few_style="""
 
 st.title("Predictive Maintainance")
 st.subheader("By team Blizzard")
-temp = False
-with st.form("myform1",clear_on_submit=True):
-  voltage = st.text_input("Enter Voltage")
-  rotation = st.text_input("Enter Rotation")
-  Pressure = st.text_input("Enter Pressure")
-  vibration = st.text_input("Enter Vibration")
-  btn = st.form_submit_button("Submit")
+# temp = False
+# with st.form("myform1",clear_on_submit=True):
+#   voltage = st.text_input("Enter Voltage")
+#   rotation = st.text_input("Enter Rotation")
+#   Pressure = st.text_input("Enter Pressure")
+#   vibration = st.text_input("Enter Vibration")
+#   btn = st.form_submit_button("Submit")
 
   
-if btn:
-  temp = True
+# if btn:
+#   temp = True
 
-if temp:
-  st.markdown(few_style,unsafe_allow_html=True)
-  st.write("Machine Fails")
-  if st.button("Check For Another Value"):
-    onChangeFunc()
+# if temp:
+#   st.markdown(few_style,unsafe_allow_html=True)
+#   st.write("Machine Fails")
+#   if st.button("Check For Another Value"):
+#     onChangeFunc()
   
-def onChangeFunc():
-  temp = False
+# def onChangeFunc():
+#   temp = False
+
+
+
