e35d2eb236893522b688bdd9cc302d942484b44f
Karan Chhabra <70219414+karan883@users.noreply.github.com>
2022-11-02 13:18:01 +0530

    Update SnowflakeApp.py

--- SnowflakeApp.py
+++ SnowflakeApp.py
@@ -6,16 +6,18 @@ import pandas
 st.title("Predictive Maintainance")
 st.subheader("By team Blizzard")
 
-data = st.file_uploader("Upload input CSV file")
-myInput = pandas.read_csv(data,encoding = "utf-8")
-st.dataframe(myInput)
-# if st.button("Predict"):
-  
-
+udata = st.file_uploader("Upload input CSV file",type=["csv"])
+if udata is not None:
+    udata = pd.read_csv(udata)
+submit = st.button('Predict')
 
 def forcast(data):
     saved_final_lr = load_model('Final_LR_Model')
     new_prediction = predict_model(saved_final_lr, data=data)
     return new_prediction
 
-
+if submit:
+    result = forcast(udata)
+    st.dataframe(result)
+    csv_result = result.to_csv(index=False).encode('utf-8')
+    st.download_button("Download Results",csv_result,"result.csv","text/csv",key='download-csv')
