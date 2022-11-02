3b38f318f83a75af85233dc5811e9f07d9384e0b
Karan Chhabra <70219414+karan883@users.noreply.github.com>
2022-11-02 11:49:40 +0530

    Update SnowflakeApp.py

--- SnowflakeApp.py
+++ SnowflakeApp.py
@@ -18,10 +18,10 @@ st.title("Predictive Maintainance")
 st.subheader("By team Blizzard")
 temp = False
 with st.form("myform1",clear_on_submit=True):
-  voltage = st.text_input("Enter Voltage",on_change=onChangeFunc)
-  rotation = st.text_input("Enter Rotation",on_change=onChangeFunc)
-  Pressure = st.text_input("Enter Pressure",on_change=onChangeFunc)
-  vibration = st.text_input("Enter Vibration",on_change=onChangeFunc)
+  voltage = st.text_input("Enter Voltage")
+  rotation = st.text_input("Enter Rotation")
+  Pressure = st.text_input("Enter Pressure")
+  vibration = st.text_input("Enter Vibration")
   btn = st.form_submit_button("Submit")
 
   
@@ -32,5 +32,5 @@ if temp:
   st.markdown(few_style,unsafe_allow_html=True)
   st.write("Machine Fails")
   
-def onChangeFunc():
-  temp = False
+# def onChangeFunc():
+#   temp = False
