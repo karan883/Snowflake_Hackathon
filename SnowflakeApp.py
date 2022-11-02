6b1cd3fb6739f97b4a3b506e6f9bbf2a8be97525
Karan Chhabra <70219414+karan883@users.noreply.github.com>
2022-11-02 10:53:24 +0530

    Update SnowflakeApp.py

--- SnowflakeApp.py
+++ SnowflakeApp.py
@@ -1,1 +1,9 @@
+import streamlit as st
+st.title("Predictive Maintainance")
+st.subheader("By team Blizzard")
 
+with st.form():
+  voltage = st.text_input("Enter Voltage")
+  rotation = st.text_input("Enter Rotation")
+  Pressure = st.text_input("Enter Pressure")
+  vibration = st.text_input("Enter Vibration")
